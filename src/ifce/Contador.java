6b91d84fa86c9a0855a9d53bc2a001c8e0aa125f
Kat2 <katysombra@gmail.com>
2014-10-14 22:54:05 -0300

    Atualização do Contador

--- src/ifce/Contador.java
+++ src/ifce/Contador.java
@@ -11,5 +11,13 @@ public class Contador {
 		}
 		return texto;
 	}
+	public String ContaImpares(int num){
+		int i;
+		String texto = "números impares";
+		for(i = 1; i<= num; i = i+2){
+			System.out.println(i);
+		}
+		return texto;
+	}
 }
 
