8f1065752856cb604818a8c1ca88b5e791143a7b
Kat2 <katysombra@gmail.com>
2014-10-14 22:40:54 -0300

    second commit

---
+++ src/ifce/Contador.java
@@ -0,0 +1,33 @@
+package ifce;
+
+
+
+public class Contador {
+	public String ContaPares(int num){
+		int i;
+		String texto = "números pares";
+		for(i = 0; i<= num; i = i+2){
+			System.out.println(i);
+		}
+		return texto;
+	}
+	
+	public String ContaImpares(int num){
+		int i;
+		String texto = "números impares";
+		for(i = 1; i<= num; i = i+2){
+			System.out.println(i);
+		}
+		return texto;
+	}
+	
+	public String ContaTudo(int num){
+		int i;
+		String texto = "números";
+		for(i = 0; i<= num; i++){
+			System.out.println(i);
+		}
+		return texto;
+	}
+}
+
---
+++ src/ifce/main.java
@@ -0,0 +1,12 @@
+package ifce;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Contador c = new Contador();
+		int num = 100;
+		c.ContaPares(num);
+	}
+
+}
